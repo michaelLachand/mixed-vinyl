d0b11112a24ece2ff44792b72ff4ac8cf292d195
Michael Lachand <lachand_michael@hotmail.com>
2025-02-26 12:08:23 +0100

    Add VinylController

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\String\u;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Title: "PB and Jams"');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+        //return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
+    }
+}
