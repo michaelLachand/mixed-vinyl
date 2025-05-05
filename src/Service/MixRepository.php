bec483ec7c5f324320e59ed3daddc5cb6f2d5c3d
Michael Lachand <lachand_michael@hotmail.com>
2025-05-05 16:12:19 +0200

    Creating service MixRepository

---
+++ src/Service/MixRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MixRepository
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly CacheInterface $cache
+    ) { }
+
+    public function findAll(): array
+    {
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
+            $cacheItem->expiresAfter(5);
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+
+            return $response->toArray();
+        });
+    }
+}
